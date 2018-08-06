757a810fe91cb7d7948056c7f422d451eeaa0f6c
Raul <raul.aponte.m@gmail.com>
2018-08-06 11:09:38 -0500

    First commit

---
+++ DevComms/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DevComms
+//
+//  Created by Raúl Aponte on 06/08/18.
+//  Copyright © 2018 DevCommsGdl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '9.0'
+
+target 'DevComms' do
+  use_frameworks!
+
+end
